05d1d08d3dd2c8582522b08bc32bb8b7d64e05aa
William Walsh <wmdwalsh0@gmail.com>
2021-08-26 13:52:58 -0400

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,28 @@
+class Book
+
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+    end
+    def author_name(author)
+        @author = name
+    end
+    def page_count
+        @page_count = 272
+    end
+    def genre
+        @genre = "Mystery"
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+    # def title 
+    #     @title
+    # end
+    # def author_name
+    #     @author_name
+    # end
+    
+end
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
 # Make your shoe class here!
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand)
+        @brand = brand
+    end
+    def what_color(color)
+        @color = x
+    end
+    def what_material
+        @material = x
+    end
+    def what_condition
+        @condition = "new"
+    end
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+        
+    end
+    
+    # def title 
+    #     @title
+    # end
+    # def color
+    #     @color
+    # end
+    
+end
